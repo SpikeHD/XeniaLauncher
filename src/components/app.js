03102897bc27f781cbb65ce2d2a380655d05b904
SpikeHD <spikegdofficial@gmail.com>
2022-03-29 23:53:39 -0700

    Reduce template to basics

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,8 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
-
-import Header from './header';
-
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+//import { h } from 'preact';
 
 const App = () => (
 	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
+		<p>Hello from Neutralino and Preact!</p>
 	</div>
 )
 
